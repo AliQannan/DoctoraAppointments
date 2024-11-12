2d7ac28b0adbcb171ccd59093ec1dd87209c8fce
aliqannan <aliqannan2003f@gmail.com>
2024-11-12 14:25:38 +0200

    add commit

--- Backend/controllers/doctorController.js
+++ Backend/controllers/doctorController.js
@@ -36,7 +36,7 @@ const loginDoctor = async(req ,res)=>{
       }
       const isMatch = await bcrypt.compare(password, doctor.password);
       if(isMatch){
-        const token = jwt.sign({id:doctor._id},process.env.JWT_SECRET)
+        const token = jwt.sign({id:doctor._id},"aliqannan")
         res.json({success:true ,token})
       }else{
         res.json({success:false,message :"Invalid credentials"})
