0901429ecc27517c2dc30ecef6ea2b0f71fa1e08
aliqannan <aliqannan2003f@gmail.com>
2024-11-27 11:24:33 +0200

    okey

--- Backend/models/adminModel.js
+++ Backend/models/adminModel.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const adminSchema = new mongoose.Schema({
-    name : {type:String , required : true },
-    email : {type : String , required : true , unique:true},
-    password: {type : String , required : true},
+    name : {type:String , required : true ,default : 'admin' },
+    email : {type : String , required : true , unique:true , default : 'admin@gmail.com'},
+    password: {type : String , required : true , default : "$2b$10$JEGbul8WTPedej8OKRgbMOxXzwE4GcTW2ylVRAe01EQ78cwTVgVyq"},
    
     //  address : {type : Object ,default : {line1 :"" , line2:""}} ,
     //  gender : {type : String , default:'Not Selected'},
