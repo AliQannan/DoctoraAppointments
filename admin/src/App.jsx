ea978235cfe8cc7b709f123cc41dc674a0a24d3b
AliQannan <126164190+AliQannan@users.noreply.github.com>
2024-12-10 13:37:54 +0200

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import Login from './pages/Login'
-import {ToastContainer, toast} from 'react-toastify';
+import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import { AdminContext  } from './context/adminContext';
 import { doctorContext  } from './context/doctorContext.jsx';
